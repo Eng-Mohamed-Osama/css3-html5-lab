7e21ab1234a09da6eccd9ee9ccc5b3022c83a53d
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-02-17 14:55:49 +0200

    responsive update

--- plugin/script.js
+++ plugin/script.js
@@ -1,5 +1,9 @@
 let navBar=document.querySelector('.navbar')
 let scrollToTopButton=document.querySelector('#scrollTop')
+let showNavBarButton=document.querySelector('#showNav')
+let navBarList=document.querySelector('.navbar__links')
+let chevronUp ='fa-chevron-up'
+let chevronDown = 'fa-chevron-down'
 
 function changeNavbarBG(){
     if(window.pageYOffset > 0){
@@ -15,8 +19,24 @@ function scrollToTopHandle (){
 
 }
 
+function showNavBar(){
+    if(navBarList.classList.contains('d-flex')){
+        document.querySelector('#showNav i').classList.add(`${chevronUp}`)
+        document.querySelector('#showNav i').classList.remove(`${chevronDown}`)
+        navBarList.classList.add('show__navbar')
+        navBar.classList.add("show__bg")
+        navBarList.classList.remove('d-flex')
+    }else{
+        document.querySelector('#showNav i').classList.remove(`${chevronUp}`)
+        document.querySelector('#showNav i').classList.add(`${chevronDown}`)
+        navBarList.classList.remove('show__navbar')
+        navBar.classList.remove("show__bg")
+        navBarList.classList.add('d-flex')
+    }
+}
 
 
 scrollToTopButton.addEventListener('click' , scrollToTopHandle)
+showNavBarButton.addEventListener('click' , showNavBar)
 
 window.addEventListener('scroll' , changeNavbarBG)
